e4f396d8058a1298f667cdb8a5835b97eb7cdbfe
epsilonv <378343+clarityD@users.noreply.github>
2021-08-26 11:03:13 -0700

    Add to setting.py

--- settings.py
+++ settings.py
@@ -1,7 +1,16 @@
+# imports
+import os
+import yaml
+from pathlib import Path
 
 
 class Settings:
-
-    def __init__(self):
+    _default_file = Path
+    def __init__(self, config_file=None):
         self.settings_file = ""
+
+    def ParseSettings(self):
+        pass
+
+
         
